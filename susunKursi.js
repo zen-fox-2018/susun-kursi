0d8957921fb5ad1018f327428bb13278e78225e2
maria <msmariaulfa33@gmail.com>
2018-11-27 18:43:05 +0700

    submit

--- susunKursi.js
+++ susunKursi.js
@@ -12,22 +12,22 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i=0; i<=row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
-
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i=0; i<arr.length; i++) {
+		// console.log(row);
+		seats[row].push(arr[i])
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
 	}
 	printSeats(seats)
@@ -35,7 +35,7 @@ function managePerson(arr, rowSeats) {
 
 function printSeats(seats) {
 	for(let i=1; i<seats.length; i++) {
-		console.log(`Baris ${i} : `, seats[i])
+		console.log(`Baris ${i} : `, seats[i-1])
 	}
 }
 
