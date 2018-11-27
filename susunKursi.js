a682f9c92305dc406275a1113a5a2928c3c6eaa9
maitreya <bluespider529@gmail.com>
2018-11-27 16:26:11 +0700

    done susun kursi

--- susunKursi.js
+++ susunKursi.js
@@ -12,29 +12,29 @@
 
 function generateSeats(row) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i = 0; i < row; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i = 0; i < arr.length; i++) {
+		seats[row].push(arr[i])
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
+	for(let i = 0; i < seats.length; i++) {
 		console.log(`Baris ${i} : `, seats[i])
 	}
 }
