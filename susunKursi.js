494f81257afdf6479300a7e2cc8caa10c02db151
patriagani <patria.gani@gmail.com>
2018-11-27 11:07:40 +0700

    susun kursi done

--- susunKursi.js
+++ susunKursi.js
@@ -10,32 +10,32 @@
 
 'use strict'
 
-function generateSeats(row) {
+function generateSeats(rows) {
 	let seats = []
-	for(let i=0; i<rowa; i++) {
-		seat.push([])
+	for(let i = 0; i < rows; i++) {
+		seats.push([])
 	}
 	return seats
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seats = generateSeats(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
-		seat[row].push(arr[i])
-		if(row <= 2) {
+	for(let i = 0; i < arr.length; i++) {
+		seats[row].push(arr[i])
+		if(row < rowSeats-1) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
 	}
 	printSeats(seats)
 }
 
 function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
-		console.log(`Baris ${i} : `, seats[i])
+	for(let i = 0; i < seats.length; i++) {
+		console.log(`Baris ${i+1} : `, seats[i])
 	}
 }
 
