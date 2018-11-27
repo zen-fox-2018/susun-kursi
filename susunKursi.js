6dc7861996368037045137c3785cdb8806855980
Aditya Respati <AdityaRespati>
2018-11-27 18:01:04 +0700

    debug

--- susunKursi.js
+++ susunKursi.js
@@ -10,33 +10,41 @@
 
 'use strict'
 
-function generateSeats(row) {
-	let seats = []
-	for(let i=0; i<rowa; i++) {
+function generateSeat(row) {
+	let seat = []
+	for(let i=0; i<row; i++) {
 		seat.push([])
 	}
-	return seats
+	// seat[0].push('')
+	// console.log(seat[0])
+	return seat
+
 }
 
 function managePerson(arr, rowSeats) {
-	let seats = generateSeat(rowSeats)
+	let seat = generateSeat(rowSeats)
 	let row = 0
 
-	for(let i=0; i<arr.length-1; i++) {
+	for(let i=0; i<arr.length; i++) {
+		// console.log(seat, row)
 		seat[row].push(arr[i])
-		if(row <= 2) {
+		if(row < 2) {
 			row++
 		} else {
-			row == 0
+			row = 0
 		}
+		// console.log(seat, row)
 	}
-	printSeats(seats)
+
+	printSeats(seat)
 }
 
-function printSeats(seats) {
-	for(let i=1; i<seats.length; i++) {
-		console.log(`Baris ${i} : `, seats[i])
+function printSeats(seat) {
+	// console.log(seat)
+	for(let i=0; i<seat.length; i++) {
+		console.log(`Baris ${i} : `, seat[i])
 	}
+	
 }
 
 managePerson(['a','b','c','d','e','f','g','h','i','j'], 3)
